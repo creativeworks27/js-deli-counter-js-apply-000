57f89314dac3aa4cfc224453a00a83237f4d94cf
Roberto Romero <creativeworks27@icloud.com>
2018-09-14 19:04:21 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,12 +1,19 @@
-var katzDeliLine = ["Steven", "Blake", "Avi"]
+var katzDeliLine = ["Steven", "Blake", "Avi"];
+var name = "Ada";
+
 function takeANumber(katzDeliLine, name) {
+  
  if (katzDeliLine.length > 0)
+ 
   katzDeliLine.push(name);
+  
   return("Welcome, " + name + ". You are number " + katzDeliLine.length + " in line.");
 }
 
 function nowServing(katzDeliLine) {
+  
   if (katzDeliLine.length === 0)
+  
   return("There is nobody waiting to be served!");
 }
 function currentLine(katzDeliLine) {
