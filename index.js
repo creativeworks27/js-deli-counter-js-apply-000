69d9395548d8c6172b85833476e993255ebcbde5
Roberto Romero <creativeworks27@icloud.com>
2018-09-14 17:40:53 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 function takeANumber(katzDeliLine, name) {
- if (katzDeliLine.length > 0)
+ if (katzDeliLine.length > 1)
   katzDeliLine.push(name);
-  return("Welcome, " + name + ". You are number " + katzDeliLine.length + "in line.");
+  return("Welcome, " + name + ". You are number " + katzDeliLine.length + " in line.");
 }
